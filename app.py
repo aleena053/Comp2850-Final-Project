c626c8b76475b53c12f7f3a74db1efeb8743058d
Aleena Anwar <nsrp2256@leeds.ac.uk>
2026-04-30 11:20:13 +0000

    function to add new user to users table

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mysql.connector
 app = Flask(__name__)
 app.secret_key = 'fitness_secret'
 
-# Connects to the database named in your schema
+# connects to database
 db = mysql.connector.connect(
     host="localhost",
     user="your_username",
@@ -18,3 +18,18 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/register', methods=['POST'])
+def register():
+    # extracting data from the HTML form
+    name = request.form.get('Name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    level = request.form.get('fitness_level') # from dropdown selection
+
+    cursor = db.cursor()
+    # matches the users table structure in the database
+    sql = "INSERT INTO Users (Name, email, password, fitness_level, role) VALUES (%s, %s, %s, %s, %s)"
+    cursor.execute(sql, (name, email, password, level, 'casual'))
+    db.commit()
+    return redirect('/dashboard')
